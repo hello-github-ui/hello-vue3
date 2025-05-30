5a9c63454b987fcd2847040cdd72ddd5a640d0e9
hello-github-ui <util.you.come@gmail.com>
2025-05-30 11:11:36 +0800

    安装vite-plugin-vue-setup-extend

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 引入 vite-plugin-vue-setup-extend 插件
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        VueSetupExtend(), // 使用
     ],
     resolve: {
         alias: {
